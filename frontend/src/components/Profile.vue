e3198ee149491058ab022bf3dff39d2ae2af6332
StephanieJ10 <mills.stephanie.j@gmail.com>
2019-01-27 11:15:15 -0500

    style changes

--- frontend/src/components/Profile.vue
+++ frontend/src/components/Profile.vue
@@ -1,78 +1,64 @@
 <template>
-    <v-app>   
-        <navigation/>
+  <v-app>
+    <navigation/>
     <v-container fill-height fluid grid-list-xl>
-        <v-layout justify-center wrap>
-            <v-flex md3></v-flex>
-            <v-flex xs12 md6>
-                <span style=" line-height: 80px; font-family: TypoPRO Bebas Neue; font-size: 40px;">Edit your profile</span>
-                <v-card dark >    
-                    <v-form>
-                        <v-container py-0>
-                            <v-layout wrap>
-                                <v-flex xs12 md4>
-                                    <v-text-field label="Company (disabled)" disabled />
-                                </v-flex>
-                                <v-flex xs12 md4>
-                                    <v-text-field class="purple-input" label="User Name" />
-                                </v-flex>
-                                <v-flex xs12 md4>
-                                    <v-text-field label="Email Address" class="purple-input" />
-                                </v-flex>
-                                <v-flex xs12 md6>
-                                    <v-text-field label="First Name" class="purple-input" />
-                                </v-flex>
-                                <v-flex xs12 md6>
-                                    <v-text-field label="Last Name" class="purple-input" />
-                                </v-flex>
-                                <v-flex xs12 md12>
-                                    <v-text-field label="Adress" class="purple-input" />
-                                </v-flex>
-                                <v-flex xs12 md4>
-                                    <v-text-field label="City" class="purple-input" />
-                                </v-flex>
-                                <v-flex xs12 md4>
-                                    <v-text-field label="Country" class="purple-input" />
-                                </v-flex>
-                                <v-flex xs12 md4>
-                                    <v-text-field class="purple-input" label="Postal Code" type="number" />
-                                </v-flex>
-                                <v-flex xs12>
-                                    <v-textarea class="purple-input" label="About Me" value="Lorem ipsum dolor sit amet, consectetur adipiscing elit." />
-                                </v-flex>
-                                <v-flex xs12 text-xs-right>
-                                    <v-btn class="mx-0 font-weight-light" color="success">
-                                        Update Profile
-                                    </v-btn>
-                                </v-flex>
-                            </v-layout>
-                        </v-container>
-                    </v-form>
-                </v-card>
-            </v-flex>
-            <v-flex xs12 md3>
-                <v-card class="v-card-profile" style="margin-top:70px" dark>
-                    <v-card-text class="text-xs-center">
-                        <h6 class="category text-gray font-weight-thin mb-3">CEO / CO-FOUNDER</h6>
-                        <h4 class="card-title font-weight-light">Alec Thompson</h4>
-                        <p class="card-description font-weight-light">Don't be scared of the truth because we need to
-                            restart the human foundation in truth And I love you like Kanye loves Kanye I love Rick
-                            Owens’ bed design but the back is...</p>
-                        <v-btn color="success" round class="font-weight-light">Follow</v-btn>
-                    </v-card-text>
-                </v-card>
-            </v-flex>
-        </v-layout>
+      <v-layout justify-center wrap>
+        <v-flex md3></v-flex>
+        <v-flex xs12 md8>
+          <span style=" line-height: 80px; font-family: Futura; font-size: 40px;">Edit your profile</span>
+          <v-card light style=" margin-top: 40px">
+            <v-form>
+              <v-container py-0>
+                <v-layout wrap>
+                  <v-flex xs12 md4>
+                    <v-text-field label="Company"/>
+                  </v-flex>
+                  <v-flex xs12 md4>
+                    <v-text-field class="purple-input" label="User Name"/>
+                  </v-flex>
+                  <v-flex xs12 md4>
+                    <v-text-field label="Email Address" class="purple-input"/>
+                  </v-flex>
+                  <v-flex xs12 md6>
+                    <v-text-field label="First Name" class="purple-input"/>
+                  </v-flex>
+                  <v-flex xs12 md6>
+                    <v-text-field label="Last Name" class="purple-input"/>
+                  </v-flex>
+                  <v-flex xs12 md12>
+                    <v-text-field label="Adress" class="purple-input"/>
+                  </v-flex>
+                  <v-flex xs12 md4>
+                    <v-text-field label="City" class="purple-input"/>
+                  </v-flex>
+                  <v-flex xs12 md4>
+                    <v-text-field label="Country" class="purple-input"/>
+                  </v-flex>
+                  <v-flex xs12 md4>
+                    <v-text-field class="purple-input" label="About me" type="number"/>
+                  </v-flex>
+                  <v-flex xs12>
+                    <v-textarea class="purple-input" label="About Me"/>
+                  </v-flex>
+                  <v-flex xs12 text-xs-right>
+                    <v-btn class="mx-0 font-weight-light" color="#004B91" dark>Update Profile</v-btn>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-form>
+          </v-card>
+        </v-flex>
+      </v-layout>
     </v-container>
-    </v-app>
+  </v-app>
 </template>
 
 <script>
-    import Navigation from './Navigation';
-    export default {
-        name: 'Profile',
-        components: {
-            navigation: Navigation,
-        }
-    }
+import Navigation from "./Navigation";
+export default {
+  name: "Profile",
+  components: {
+    navigation: Navigation
+  }
+};
 </script>
--- frontend/src/components/Templates.vue
+++ frontend/src/components/Templates.vue
@@ -1,52 +1,51 @@
 <template>
-<v-app>
-    <navigation />
-<v-content style="padding: 20px; margin-left: 400px">
-            <span class="head">Templates</span>
-            <v-container fluid>
-                <v-layout row wrap>
-                    <v-card>
-                        <v-img src="https://cdn.vuetifyjs.com/images/cards/desert.jpg" aspect-ratio="2.75"></v-img>
+  <v-app>
+    <navigation/>
+    <v-content style="padding: 20px; margin-left: 400px">
+      <span class="head">Templates</span>
+      <v-container fluid>
+        <v-layout row wrap>
+          <v-card>
+            <v-img src="https://cdn.vuetifyjs.com/images/cards/desert.jpg" aspect-ratio="2.75"></v-img>
 
-                        <v-card-title primary-title>
-                            <div>
-                                <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
-                            </div>
-                        </v-card-title>
-                    </v-card>
-                    <v-card>
-                        <v-img src="https://cdn.vuetifyjs.com/images/cards/desert.jpg" aspect-ratio="2.75"></v-img>
+            <v-card-title primary-title>
+              <div>
+                <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+              </div>
+            </v-card-title>
+          </v-card>
+          <v-card>
+            <v-img src="https://cdn.vuetifyjs.com/images/cards/desert.jpg" aspect-ratio="2.75"></v-img>
 
-                        <v-card-title primary-title>
-                            <div>
-                                <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
-                            </div>
-                        </v-card-title>
-                    </v-card>
-                </v-layout>
-            </v-container>
-        </v-content>
-</v-app>
+            <v-card-title primary-title>
+              <div>
+                <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+              </div>
+            </v-card-title>
+          </v-card>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 import Navigation from "./Navigation";
 export default {
-    name: 'Templates',
-    components: {
-        navigation: Navigation,
-    },
-}
+  name: "Templates",
+  components: {
+    navigation: Navigation
+  }
+};
 </script>
 
 <style scoped>
 .head {
-    font-family: TypoPRO Bebas Neue; 
-    font-size: 45px;
-    
+  font-family: Futura;
+  font-size: 45px;
 }
 .v-card {
-    margin: 20px;
-    width: 400px;
+  margin: 20px;
+  width: 400px;
 }
 </style>
