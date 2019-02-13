d4bc5a6e0e47d6667582f3f08bac466da795d7df
Talent Zeng <talent.z@hotmail.com>
2019-02-12 20:51:30 -0500

    Changed the login page to a REAL page

--- frontend/src/views/login.vue
+++ frontend/src/views/login.vue
@@ -1,34 +1,28 @@
-
-
 <template>
   <v-app light>
     <link
       href="https://cdn.jsdelivr.net/npm/@typopro/web-bebas-neue@3.7.5/TypoPRO-BebasNeue-Bold.min.css"
-      rel="stylesheet"
-    >
-    <v-content>
-      <v-container fluid fill-height>
-        <v-layout align-center justify-left>
-          <v-flex xs12 sm8 md4>
-            <img src="../assets/agilite-logo.svg"></img>
-            <span
-              style="margin-left: 10px; margin-bottom: 10px; position: absolute; font-family: Futura; color: #303030; font-size: 70px; text-align: center;"
-            >Agilite</span>
-            
-              <!-- <v-toolbar dark color="primary">
-                <v-toolbar-title>Login </v-toolbar-title>
-                <v-spacer></v-spacer>
-              </v-toolbar>-->
-              <v-card-text>
-                <v-form >
-                  <v-text-field 
+      rel="stylesheet">
+      <b-row>
+          <b-col cols="4">
+            <div class="sign-in-form">
+              <b-row class="full-logo">
+                <b-col>
+                    <img class="logo" src="../assets/agilite-logo.svg"></img>
+                </b-col>
+                <b-col>
+                    <span class="agilite-name">Agilite</span>
+                </b-col>
+              </b-row>
+              <v-form>
+                  <v-text-field
                     prepend-icon="person"
                     name="login"
                     label="Email"
                     type="text"
                     v-model="input.username"
                   ></v-text-field>
-                  <v-text-field 
+                  <v-text-field
                     id="password"
                     prepend-icon="lock"
                     name="password"
@@ -36,36 +30,27 @@
                     v-model="input.password"
                     type="password"
                   ></v-text-field>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="#004B91" v-on:click="login()" dark>Login</v-btn>
+                  </v-card-actions>
                 </v-form>
-              </v-card-text>
-              <v-card-actions style=" margin-bottom: 220px; ">
-                <v-spacer></v-spacer>
-                <v-btn color="#004B91" v-on:click="login()" dark>Login</v-btn>
-              </v-card-actions>
-          
-            <!-- <v-card class="login" style="margin-top: 40px; text-align: center; font-weight: 600">
-              <span style="font-family: Roboto; font-size: 18px">Register | Forgot Password?</span>
-            </v-card>-->
-          </v-flex>
-        </v-layout>
-        <v-layout align-center>
-          <v-flex md4>
-            <!-- <v-card class="login">
-              <span
-                style="font-family: TypoPRO Bebas Neue; font-size: 40px;"
-              >Build Fast.Document Faster.</span>
-            </v-card>
-            <v-card class="login trans" style="margin-top: 15px;">
-              <span>Make your brainstorming and design sessions last with advanced image recognition and digital manipulation</span>
-            </v-card>-->
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-content>
+            </div>
+            <div class="text-center">
+              <span class="credentials-info">Register</span>
+              <span class="credentials-info"> | Forgot password?</span>
+            </div>
+          </b-col>
+          <b-col cols="8" class="bg_img">
+              <div class="slogan">
+                  <h1>Build fast.</h1>
+                  <h1>Document faster.</h1>
+                  <p style="width: 285px">Digitalizing your agile brainstorming sessions with advanced image recognition so you can <b>sprint</b> faster.</p>
+              </div>
+          </b-col>
+      </b-row>
   </v-app>
 </template>
-
-
 
 <script>
 const axios = require("axios");
@@ -81,11 +66,10 @@ export default {
   },
   methods: {
     login() {
-      if (this.input.username != "" && this.input.password != "") {
-    
+      if (this.input.username !== "" || this.input.password !== "") {
         if (
-          this.input.username == this.$parent.mockAccount.username &&
-          this.input.password == this.$parent.mockAccount.password
+          this.input.username === this.$parent.mockAccount.username &&
+          this.input.password === this.$parent.mockAccount.password
         ) {
           this.$emit("authenticated", true);
           this.$router.replace({ name: "dashboard" });
@@ -105,25 +89,66 @@ div.v-card__text {
   margin-left: 85px;
   padding-right: 0px;
 }
+
+.slogan {
+  color: white;
+  margin-top: 15%;
+  padding: 30px;
+}
 .trans {
   background-color: none;
 }
 
+.credentials-info {
+  font-family: Roboto;
+  font-size: 18px;
+}
+
+.credentials-info:hover {
+  cursor: pointer;
+  color: blue;
+}
+
+.text-center {
+  text-align: center;
+}
+
+.bg_img {
+  background-image: url("../assets/bg_pic2.jpg");
+  background-size: cover;
+  height: -webkit-fill-available;
+}
+
+.agilite-name {
+  font-family: Futura;
+  font-size: 70px;
+}
+
+.logo {
+  height: 100px;
+  padding-left: 30px;
+}
+
+.full-logo {
+  margin-bottom: 40px;
+}
+
+.sign-in-form {
+  padding: 40px;
+}
+
 .login {
   background-color: white;
+  font-weight: 600;
   padding: 20px;
   width: 500px;
   margin-left: 5px;
   margin-bottom: 250px;
+  margin-top: 40px;
+  text-align: center;
 }
 #app {
   width: 100%;
-}
-img {
-  width:34%;
-  margin-left: 85px;
-  margin-bottom: 40px;
-  align-content: center;
 }
 
 div.v-btn__content {
@@ -134,25 +159,11 @@ div.v-btn__content {
 }
 
 div.v-card__actions {
-  width: 80%;
-  margin-left: 100px;
+  padding: 0;
 }
 
 button {
   margin-bottom: 500px;
 }
 
-.container {
-  background-image: url("../assets/login-bg@2x.jpg");
-  background-size: cover;
-  /* background-size: cover, */
-  /* linear-gradient(s
-      to right,
-      #303030 0%,
-      #303030 50%,
-      #abdcff 50%,
-      #0f64c3 51%,
-      #09396b 90%
-    ); */
-}
 </style>
